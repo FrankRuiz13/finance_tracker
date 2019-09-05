eb0f3211904d808d779966abaa6ade09920a1bd7
Frank Ruiz <frankruiz49@gmail.com>
2019-09-05 14:42:43 -0700

    added error message fucntionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,18 @@ class StocksController < ApplicationController
 
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:warning] = "You have entered and incorrect symbol"
+        redirect_to root_path
+      end
+    else
+      flash[:warning] = "You have entered an empty search string"
+      redirect_to root_path
+    end
 
   end
 
